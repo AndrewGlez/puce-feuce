7e04507272b8d5fbf643e4bdade25929fe093f17
AndrewGlez <andrewgonzale009@gmail.com>
2025-06-19 11:53:46 -0500

    first commit

---
+++ src/server/index.ts
@@ -0,0 +1,53 @@
+import express, {
+  type ErrorRequestHandler,
+  urlencoded,
+  json,
+  type Request,
+  type Response,
+} from "express";
+import { ValidateError } from "tsoa";
+import { createServer as createServerVite } from "vite";
+import { RegisterRoutes } from "./build/routes";
+import swaggerUi from "swagger-ui-express";
+
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+RegisterRoutes(app);
+
+if (process.env.NODE_ENV === "development") {
+  createServerVite({
+    server: { middlewareMode: true },
+  }).then((vite) => {
+    app.use(vite.middlewares);
+  });
+}
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+    return;
+  }
+  if (err instanceof Error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+    return;
+  }
+
+  next();
+};
+
+app.use(errorHandler);
+app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
+  return res.send(swaggerUi.generateHTML(await import("./build/swagger.json")));
+});
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
